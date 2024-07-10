e777e426036ba680675f3aa5122ea6287cc5fba4
Oskar Karpiński <me@medzik.dev>
2024-07-10 22:31:04 +0200

    io: Add new `ioBlock` function with `context: CoroutineContext` parameter

--- io/src/main/kotlin/dev/medzik/common/io/Block.kt
+++ io/src/main/kotlin/dev/medzik/common/io/Block.kt
@@ -1,5 +1,8 @@
 package dev.medzik.common.io
 
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
+
 /**
  * Wraps a given [IO] block into a new [IO] instance, allowing it to be used within other suspend functions.
  *
@@ -9,7 +12,25 @@ package dev.medzik.common.io
  * @sample dev.medzik.common.io.BlockTest.blockSample
  */
 inline fun <T> ioBlock(
-    crossinline block: IO<T>,
+    crossinline block: IO<T>
 ): IO<T> = suspend {
     block()
+}
+
+/**
+ * Wraps a given [IO] block into a new [IO] instance, allowing it to be used within other suspend functions.
+ *
+ * @param context The coroutine context to be used for wrapping the block.
+ * @param block The block to be wrapped into a new [IO] instance.
+ * @return A new [IO] instance.
+ *
+ * @sample dev.medzik.common.io.BlockTest.blockWithContextSample
+ */
+inline fun <T> ioBlock(
+    context: CoroutineContext,
+    crossinline block: IO<T>
+): IO<T> = ioBlock {
+    withContext(context) {
+        block()
+    }
 }
--- io/src/test/kotlin/dev/medzik/common/io/BlockTest.kt
+++ io/src/test/kotlin/dev/medzik/common/io/BlockTest.kt
@@ -1,5 +1,6 @@
 package dev.medzik.common.io
 
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -8,6 +9,15 @@ class BlockTest {
     @Test
     fun blockSample() = runBlocking {
         val result = ioBlock {
+            "Hello, World!"
+        }
+
+        assertEquals("Hello, World!", result())
+    }
+
+    @Test
+    fun blockWithContextSample() = runBlocking {
+        val result = ioBlock(Dispatchers.IO) {
             "Hello, World!"
         }
 
