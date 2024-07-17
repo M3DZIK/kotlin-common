8462f5f3cc80cc51e143f9718aae6787599cec9c
Oskar Karpiński <me@medzik.dev>
2024-07-17 22:16:37 +0200

    Add export `dev.medzik.common.extensions` to `module-info.java`

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,6 @@ module dev.medzik.common {
     requires transitive kotlin.stdlib;
     requires kotlinx.coroutines.core;
 
+    exports dev.medzik.common.extensions;
     exports dev.medzik.common.io;
 }
