a07d5cb9e661e9799c6710f009dab2e30a3607cf
Oskar Karpiński <me@medzik.dev>
2024-07-20 16:48:54 +0200

    Make functions `parallelMap` and parallelForEach` as inline function

--- src/main/kotlin/dev/medzik/common/io/Parallel.kt
+++ src/main/kotlin/dev/medzik/common/io/Parallel.kt
@@ -8,12 +8,12 @@ import kotlinx.coroutines.coroutineScope
  * Performs a parallel mapping operation on the elements of this iterable collection using the provided [f] function.
  * The [f] function is invoked on each element in a separate coroutine, allowing for concurrent execution.
  *
- * @param f The function to apply to each element. This function must be a suspend function.
+ * @param f The function to apply to each element.
  * @return A list containing the results of applying the [f] function to each element in this iterable collection.
  *
  * @sample dev.medzik.common.io.ParallelTest.parallelMapSample
  */
-suspend fun <T, R> Iterable<T>.parallelMap(f: suspend (T) -> R): List<R> = coroutineScope {
+suspend inline fun <T, R> Iterable<T>.parallelMap(crossinline f: suspend (T) -> R): List<R> = coroutineScope {
     map { async { f(it) } }.awaitAll()
 }
 
@@ -21,10 +21,10 @@ suspend fun <T, R> Iterable<T>.parallelMap(f: suspend (T) -> R): List<R> = corou
  * Performs a parallel iteration over the elements of this iterable collection using the provided [f] function.
  * The [f] function is invoked on each element in a separate coroutine, allowing for concurrent execution.
  *
- * @param f The function to apply to each element. This function must be a suspend function.
+ * @param f The function to apply to each element.
  *
  * @sample dev.medzik.common.io.ParallelTest.parallelForEachSample
  */
-suspend fun <T> Iterable<T>.parallelForEach(f: suspend (T) -> Unit) {
+suspend inline fun <T> Iterable<T>.parallelForEach(crossinline f: suspend (T) -> Unit) {
     parallelMap(f)
 }
